0c7fce9b7b87f3df081bd40a94a95823ae145cee
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2026-01-21 12:26:24 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/Kushagra/SpringBoot_Web_App/SpringBootWeb1Application.java
@@ -0,0 +1,13 @@
+package com.Kushagra.SpringBoot_Web_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWeb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWeb1Application.class, args);
+	}
+
+}
