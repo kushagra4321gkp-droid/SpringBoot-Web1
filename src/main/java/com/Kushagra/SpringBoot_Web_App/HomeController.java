b4a0c79a0f41b4fc0cfc0d6700469e0b2d5b8b34
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2026-01-21 15:49:43 +0530

    Created a new Controller and in that -> Accepting Data the Servlet way

--- src/main/java/com/Kushagra/SpringBoot_Web_App/HomeController.java
+++ src/main/java/com/Kushagra/SpringBoot_Web_App/HomeController.java
@@ -1,5 +1,6 @@
 package com.Kushagra.SpringBoot_Web_App;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,5 +11,18 @@ public class HomeController {
     public String home(){
         System.out.println("Home Method Called...");
         return "index.jsp";
+    }
+
+    @RequestMapping("add")
+    public String add(HttpServletRequest req){
+
+        System.out.println("in add");
+
+        int num1 = Integer.parseInt(req.getParameter("num1"));
+        int num2 = Integer.parseInt(req.getParameter("num2"));
+        int result = num1 + num2;
+        System.out.println(result);
+
+        return "result.jsp";
     }
 }
