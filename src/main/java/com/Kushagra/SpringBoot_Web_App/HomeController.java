c4ea6869aa5c61215175ab8712844963a6e71716
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2026-01-21 14:53:02 +0530

    Used @RequestMapping('/') for mapping the Controller(Servlet) to  the right request, but we got a html view page downloaded because our index.jsp didn't get converted into servlet, to do so we had to add a dependency of Tomcat Jasper whose version should be same as out embedded tomcat's version and then it worked

--- src/main/java/com/Kushagra/SpringBoot_Web_App/HomeController.java
+++ src/main/java/com/Kushagra/SpringBoot_Web_App/HomeController.java
@@ -1,12 +1,14 @@
 package com.Kushagra.SpringBoot_Web_App;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+    @RequestMapping("/")
     public String home(){
-
+        System.out.println("Home Method Called...");
         return "index.jsp";
     }
 }
