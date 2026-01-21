df86ae762d35dc90fb93b482b1aabad49ef7d5fb
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2026-01-21 20:42:32 +0530

    ModelAndView object to only work with data, you can add multiple objects in via addObject() method and setViewName()

--- src/main/java/com/Kushagra/SpringBoot_Web_App/HomeController.java
+++ src/main/java/com/Kushagra/SpringBoot_Web_App/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -17,7 +18,7 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int num, @RequestParam("num2") int num2, Model model){
+    public ModelAndView add(@RequestParam("num1") int num, @RequestParam("num2") int num2, ModelAndView mv){
 
         System.out.println("in add");
 
@@ -28,9 +29,10 @@ public class HomeController {
 
 //        session.setAttribute("result", result);
 
-        model.addAttribute("result", result);
+        mv.addObject("result", result);
+        mv.setViewName("result");
 
-        return "result";
+        return mv;
     }
 }
 
